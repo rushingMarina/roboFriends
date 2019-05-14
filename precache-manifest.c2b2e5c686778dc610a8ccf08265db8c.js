06e5e473a25b1527357ecd41f46706ed1df25179
mordva42 <333marina2007@gmail.com>
2019-05-13 22:55:15 -0700

    Updates

---
+++ precache-manifest.c2b2e5c686778dc610a8ccf08265db8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb27fd209a28bc61e0e682e83f038ca",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/roboFriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "9aadaf9304a4796e4530",
+    "url": "/roboFriends/static/css/main.a58c04f1.chunk.css"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/roboFriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "9aadaf9304a4796e4530",
+    "url": "/roboFriends/static/js/main.cb4cf46a.chunk.js"
+  },
+  {
+    "revision": "0d6209f39b04744aca3d",
+    "url": "/roboFriends/static/js/runtime~main.0e8c0065.js"
+  },
+  {
+    "revision": "4c94a6a72ad05c79be3ba1bc23c0d60d",
+    "url": "/roboFriends/static/media/FSEX300.4c94a6a7.woff"
+  }
+]);
